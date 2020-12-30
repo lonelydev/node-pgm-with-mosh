e40beb59cabe590cbe806908802e6bc35d8dc6d3
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2020-12-30 16:34:35 +0000

    intro to modules

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ Following https://www.youtube.com/watch?v=TlB_eWDSMt4
 `first-app.js` - a look at the global object. 
 
 ## Modules
+
+You need to manage code in a large application. Not every method can be defined in the global scope. 
+Thus was born modules! Modules can be viewed as a way to namespace methods.
+
+Methods and variables defined inside a module, is private to that module. It isn't exposed to the outside world, unless explicitly exported. 
+
+in node, every file is a module and functions and variables in a file are not exposed outside. 
+
+### Create a module
+
+create a file. 
---
+++ first-demo-module.js
@@ -0,0 +1,21 @@
+console.log(module);
+
+// this will print a lot of the metadata about the module:
+
+// [2020-12-30 16:32:21][/d/Code/nodejs/youtube/pgmwithmosh] (master)> node first-demo-module.js
+// Module {
+//   id: '.',
+//   path: 'D:\\Code\\nodejs\\youtube\\pgmwithmosh',
+//   exports: {},
+//   parent: null,
+//   filename: 'D:\\Code\\nodejs\\youtube\\pgmwithmosh\\first-demo-module.js',
+//   loaded: false,
+//   children: [],
+//   paths: [
+//     'D:\\Code\\nodejs\\youtube\\pgmwithmosh\\node_modules',
+//     'D:\\Code\\nodejs\\youtube\\node_modules',
+//     'D:\\Code\\nodejs\\node_modules',
+//     'D:\\Code\\node_modules',
+//     'D:\\node_modules'
+//   ]
+// }
